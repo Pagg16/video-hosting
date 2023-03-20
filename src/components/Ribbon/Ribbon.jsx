d1b62cac9da2c11ef4ffd4ccd671dbafd820523a
Pavel Kulebin <pagg16pagg@gmail.com>
2023-03-20 12:56:27 +0300

    ribbon

--- src/components/Ribbon/Ribbon.jsx
+++ src/components/Ribbon/Ribbon.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import "./ribbon.css";
 
 function Ribbon() {
-  return <div>Ribbon</div>;
+  return (
+    <Stack
+      className="ribbon"
+      sx={{ flexDirection: { sx: "column", md: "row" } }}
+    >
+      <Box
+        sx={{
+          height: {
+            sx: "auto",
+            md: "92vh",
+          },
+          borderRight: "1px solid #3d3d3d",
+          px: {
+            sx: 0,
+            md: 2,
+          },
+        }}
+      >
+        <Typography
+          className="ribbon__copiright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copiright 2023 video-hosting
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
 
 export default Ribbon;
